01525b9abfb86a096d157b9afb968ce123b88ea4
CairoBraz <kairobc@hotmail.com>
2024-05-19 11:17:51 -0300

    Encapsulamento na prática

--- Program.cs
+++ Program.cs
@@ -1,7 +1,32 @@
 ﻿using ExemploPOO.Models;
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Cairo";
-p1.Idade = 28;
+ContaCorrente c1 = new ContaCorrente(123, 1000);
 
-p1.Apresentar();
+c1.ExibirSaldo();
+c1.Sacar(600.00M);
+c1.ExibirSaldo();
+
+c1.Sacar(600.00M);
+c1.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Cairo";
+// p1.Idade = 28;
+
+// p1.Apresentar();
