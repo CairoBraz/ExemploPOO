12cecb4c663900002eae730d6c8b451b711ec86f
CairoBraz <kairobc@hotmail.com>
2024-05-19 12:10:14 -0300

    polimorfismo

--- Program.cs
+++ Program.cs
@@ -1,13 +1,43 @@
 ﻿using ExemploPOO.Models;
 
-ContaCorrente c1 = new ContaCorrente(123, 1000);
+Aluno a1 = new Aluno();
+a1.Nome = "Cairo";
+a1.Idade = 28;
+a1.Email = "kairo@hotmail.com";
+a1.Nota = 85.36;
 
-c1.ExibirSaldo();
-c1.Sacar(600.00M);
-c1.ExibirSaldo();
+a1.Apresentar();
 
-c1.Sacar(600.00M);
-c1.ExibirSaldo();
+Professor p1 = new Professor();
+p1.Nome = "Jose";
+p1.Idade = 60;
+p1.Email = "jose@email";
+p1.Salario = 7850.55M;
+
+p1.Apresentar();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+// c1.ExibirSaldo();
+// c1.Sacar(600.00M);
+// c1.ExibirSaldo();
+
+// c1.Sacar(600.00M);
+// c1.ExibirSaldo();
 
 
 
